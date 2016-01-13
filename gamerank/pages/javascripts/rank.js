047add2a45d269d792fb561cead3bca59e629003
aaaeeeo <lee.zuoming@gmail.com>
2016-01-13 09:19:46 -0600

    upload views

---
+++ gamerank/pages/javascripts/rank.js
@@ -0,0 +1,19 @@
+/**
+ * Created by fanbaolin on 16/1/10.
+ */
+function firstpage() {
+    //alert(window.location.href);
+    //window.location.href = "http://www.baidu.com"; pass
+    //window.location.href = "http://www.weixingate.com/gate.php?back=http://www.baidu.com&force=1&info=none";
+    window.location.href = "http://www.weixingate.com/gate.php?back=http://scut.win/views/second&force=1&arg=0";
+};
+
+function login(){
+    $.post()
+}
+
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r!=null) return (r[2]); return null;
+}
