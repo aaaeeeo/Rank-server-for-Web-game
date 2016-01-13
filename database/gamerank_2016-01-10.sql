a3277650ab9f73719add0a97deabcd216c4dc74b
aaaeeeo <lee.zuoming@gmail.com>
2016-01-13 12:35:04 -0600

    1.2

--- database/gamerank_2016-01-10.sql
+++ database/gamerank_2016-01-10.sql
@@ -19,6 +19,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE `gamerank` DEFAULT CHARACTER SET = `utf8mb4`;
+USE `gamerank`;
 
 # Dump of table games
 # ------------------------------------------------------------
