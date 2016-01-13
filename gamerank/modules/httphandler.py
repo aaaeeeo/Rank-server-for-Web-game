c89264352906aab140794707e7ba616c1d24823c
aaaeeeo <lee.zuoming@gmail.com>
2016-01-13 10:33:11 -0600

    test

--- gamerank/modules/httphandler.py
+++ gamerank/modules/httphandler.py
@@ -73,9 +73,9 @@ class HTTPHandler(BaseHTTPRequestHandler):
             datas = self.rfile.read(int(self.headers['content-length']))
             datas = str(datas, ENCODING)
             json_datas = urllib.parse.unquote(datas)
-            dict_datas = json.loads(json_datas)
+            #dict_datas = json.loads(json_datas)
             print(json_datas)
-            return dict_datas
+            return json_datas
 
     def __response(self, code, content="", type="text/html", btype='text'):
 
