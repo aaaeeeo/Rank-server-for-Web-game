b69f77ce5abd1d50769e0e5dd0dce7e12b6d7db5
finmily <b1457650499@qq.com>
2016-01-30 00:33:41 +0800

    app.js

--- gamerank/pages/games/memory/js/app.js
+++ gamerank/pages/games/memory/js/app.js
@@ -76,7 +76,7 @@
             if ($('.matched').length != $pieces.length) {
                 $("#confirmDialog").popup();
                 $("#confirmDialog").popup("open");
-                $("#score").text($("#score").text()+tries.toString());
+                $("#score").text(tries.toString());
                 $("#confirmDialog").trigger('create');
 
                 /*if (window.confirm('Congrats! You won with only ' + tries + ' tries. Not bad! Play again?')) {
